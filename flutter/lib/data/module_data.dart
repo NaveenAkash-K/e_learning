1c9219774e5541e5b00795e54ff20f7ce3834e43
subash_ <119630760+subash1404@users.noreply.github.com>
2024-01-20 19:37:43 +0530

    Add files via upload

--- flutter/lib/data/module_data.dart
+++ flutter/lib/data/module_data.dart
@@ -53,43 +53,277 @@ class ImageContent {
   final Image image;
   final double topPadding;
   final double bottomPadding;
+
+  Widget build(BuildContext context) {
+    // Get the screen width using MediaQuery
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: SizedBox(
+        width: screenWidth - 100, // Adjust as needed
+        child: image,
+      ),
+    );
+  }
 }
 
-final List<dynamic> module1 = [
-  ImageContent(
-    image: Image.asset(
-      "assets/images/sample/sample_2.png",
-      width: double.infinity,
-      fit: BoxFit.fitWidth,
+class TabularColumn {
+  final List<List<String>> data;
+  final TextStyle headerTextStyle;
+  final TextStyle cellTextStyle;
+
+  const TabularColumn({
+    required this.data,
+    this.headerTextStyle = const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
     ),
-  ),
-  const Heading(),
-  const SubHeading(text: "Introuction"),
-  const Body(
-    text:
-        "FMCG, or 𝗙𝗮𝘀𝘁 𝗠𝗼𝘃𝗶𝗻𝗴 𝗖𝗼𝗻𝘀𝘂𝗺𝗲𝗿 𝗚𝗼𝗼𝗱𝘀, holds a pivotal role in shaping our daily experiences. These products are the essentials we reach for regularly, from toothpaste and snacks to cleaning supplies and personal care",
-  ),
-  const Body(
-    text:
-        "items. Now, let's delve into why FMCG matters, with a particular focus on the emerging field of shelf analytics",
-  ),
-  const SubHeading(text: "Key Points"),
-  const BulletPoint(
-    text:
-        "items. Now, let's delve into why FMCG matters, with a particular focus on the emerging field of shelf analytics",
-  ),
-  const BulletPoint(
-    text:
-        "By strategically placing high-demand or complementary products at eye level and easily accessible areas, retailers can guide customers through a seamless and enjoyable shopping journey.",
-  ),
-  const SubHeading(text: "Conclusion"),
-  const Body(
-    text:
-        "A high Out-of-Stock Rate negatively impacts customer satisfaction, leading to potential revenue loss. When products are consistently unavailable, customers may turn to competitors, harming brand loyalty. Monitoring and minimizing this rate is vital for ensuring a positive customer experience and maximizing sales opportunities.",
-  ),
-  const Body(
-    text:
-        "A high Out-of-Stock Rate negatively impacts customer satisfaction, leading to potential revenue loss. When products are consistently unavailable, customers may turn to competitors, harming brand loyalty. Monitoring and minimizing this rate is vital for ensuring a positive customer experience and maximizing sales opportunities.",
-  ),
-  const SizedBox()
+    this.cellTextStyle = const TextStyle(
+      fontSize: 16,
+    ),
+  });
+
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: Table(
+        border: TableBorder.all(),
+        children: data.asMap().entries.map((entry) {
+          final rowIndex = entry.key;
+          final row = entry.value;
+          return TableRow(
+            children: row.map((cell) {
+              return TableCell(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    cell,
+                    style: rowIndex == 0 ? headerTextStyle : cellTextStyle,
+                  ),
+                ),
+              );
+            }).toList(),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
+final List<dynamic> module = [
+  {
+    'title': 'Shelf Analytics',
+    // ImageContent(
+    //   image: Image.asset(
+    //     "assets/images/sample/sample_2.png",
+    //     width: double.infinity,
+    //     fit: BoxFit.fitWidth,
+    //   ),
+    // ),
+    'content': [
+      [
+        const Heading(),
+        const SubHeading(text: "FMCG and FMCD"),
+        const Body(
+          text:
+              "FMCG -> Fast Moving Consumer Goods	\nFMCD -> Fast Moving Consumer Durables",
+        ),
+        const TabularColumn(
+          data: [
+            [
+              'FMCG',
+              'FMCD',
+            ],
+            [
+              'Personal Care products , Shampoo, Face Wash, Soap etc',
+              'Automobile , Cars, Bikes, Trucks etc'
+            ],
+            [
+              'Packaged Food & Beverages , Chips, Juices, Carbonated drinks etc',
+              'Household appliances , Washing Machine, Iron, Oven, Vacuum Cleaner etc'
+            ],
+            [
+              'Laundry , Detergent etc \nOver the counter Medicines',
+              'Electronic gadgets , Smart Phones, Laptops etc'
+            ],
+          ],
+        ),
+        const SubHeading(
+          text: "Examples of FMCG",
+        ),
+        ImageContent(
+          image: Image.asset(
+            "assets/images/module1/fmcg_eg.png",
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+        const SubHeading(
+          text: "Examples of FMCD",
+        ),
+        ImageContent(
+          image: Image.asset(
+            "assets/images/module1/fmcd_eg.png",
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+        const Body(
+          text:
+              "As per ICRA the term FMCG has been defined to include products that are consumed at least once a month, sold under the national brands in consumer packages, sold through a wide spared distribution network and consumed directly by the consumers.",
+        ),
+        const Body(
+          text:
+              "The main difference between the two is that FMCG products are consumable while FMCD products are non-durable and have a longer usage period. They also differ basis the buying decision. ",
+        ),
+        const SizedBox()
+      ],
+      [
+        const Heading(),
+        const SubHeading(text: "FMCG and FMCD"),
+        const Body(
+          text:
+              "FMCG -> Fast Moving Consumer Goods	\nFMCD -> Fast Moving Consumer Durables",
+        ),
+        const TabularColumn(
+          data: [
+            [
+              'FMCG',
+              'FMCD',
+            ],
+            [
+              'Personal Care products , Shampoo, Face Wash, Soap etc',
+              'Automobile , Cars, Bikes, Trucks etc'
+            ],
+            [
+              'Packaged Food & Beverages , Chips, Juices, Carbonated drinks etc',
+              'Household appliances , Washing Machine, Iron, Oven, Vacuum Cleaner etc'
+            ],
+            [
+              'Laundry , Detergent etc \nOver the counter Medicines',
+              'Electronic gadgets , Smart Phones, Laptops etc'
+            ],
+          ],
+        ),
+        const SubHeading(
+          text: "Examples of FMCG",
+        ),
+        ImageContent(
+          image: Image.asset(
+            "assets/images/module1/fmcg_eg.png",
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+        const SubHeading(
+          text: "Examples of FMCD",
+        ),
+        ImageContent(
+          image: Image.asset(
+            "assets/images/module1/fmcd_eg.png",
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+        const Body(
+          text:
+              "As per ICRA the term FMCG has been defined to include products that are consumed at least once a month, sold under the national brands in consumer packages, sold through a wide spared distribution network and consumed directly by the consumers.",
+        ),
+        const Body(
+          text:
+              "The main difference between the two is that FMCG products are consumable while FMCD products are non-durable and have a longer usage period. They also differ basis the buying decision. ",
+        ),
+        const SizedBox()
+      ],
+    ],
+  },
+  {
+    'title': 'iahdiasd',
+    'content': [
+      [
+        const Heading(),
+        const SubHeading(text: "FMCG and FMCD"),
+        const Body(
+          text:
+              "FMCG -> Fast Moving Consumer Goods	\nFMCD -> Fast Moving Consumer Durables",
+        ),
+        const TabularColumn(
+          data: [
+            [
+              'FMCG',
+              'FMCD',
+            ],
+            [
+              'Personal Care products , Shampoo, Face Wash, Soap etc',
+              'Automobile , Cars, Bikes, Trucks etc'
+            ],
+            [
+              'Packaged Food & Beverages , Chips, Juices, Carbonated drinks etc',
+              'Household appliances , Washing Machine, Iron, Oven, Vacuum Cleaner etc'
+            ],
+            [
+              'Laundry , Detergent etc \nOver the counter Medicines',
+              'Electronic gadgets , Smart Phones, Laptops etc'
+            ],
+          ],
+        ),
+        const SubHeading(
+          text: "Examples of FMCG",
+        ),
+        ImageContent(
+          image: Image.asset(
+            "assets/images/module1/fmcg_eg.png",
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+        const SubHeading(
+          text: "Examples of FMCD",
+        ),
+        ImageContent(
+          image: Image.asset(
+            "assets/images/module1/fmcd_eg.png",
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+        const Body(
+          text:
+              "As per ICRA the term FMCG has been defined to include products that are consumed at least once a month, sold under the national brands in consumer packages, sold through a wide spared distribution network and consumed directly by the consumers.",
+        ),
+        const Body(
+          text:
+              "The main difference between the two is that FMCG products are consumable while FMCD products are non-durable and have a longer usage period. They also differ basis the buying decision. ",
+        ),
+        const SizedBox()
+      ]
+    ]
+  },
+  {
+    'title': '',
+    'content': [[]]
+  },
+  {
+    'title': '',
+    'content': [[]]
+  },
+  {
+    'title': '',
+    'content': [[]]
+  },
+  {
+    'title': '',
+    'content': [[]]
+  },
+  {
+    'title': '',
+    'content': [[]]
+  },
+  {
+    'title': '',
+    'content': [[]]
+  },
+  {
+    'title': '',
+    'content': [[]]
+  },
+  {
+    'title': '',
+    'content': [[]]
+  },
 ];
