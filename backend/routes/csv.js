e915f1f65954d452d8a141d49ef80b3d6bd5ba08
Naveen Akash <naveen.akash0904@gmail.com>
2024-02-14 21:37:21 +0530

    Added CSV Login route

--- backend/routes/csv.js
+++ backend/routes/csv.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const csv = require('csv-parser');
+const multer = require("multer");
+const csv = require("csv-parser");
 const json2csv = require("json2csv").Parser;
 const User = require("../models/usermodel");
 const upload = multer();
@@ -26,17 +26,23 @@ router.get("/fetch", async (req, res) => {
         cs2: "Case Study 2",
         cs3: "Case Study 3",
         cs4: "Case Study 4",
-        cs5: "Case Study 5"
+        cs5: "Case Study 5",
       };
 
       // Transform the data using the fieldMapping
-      const transformedData = documents.map(document => {
+      const transformedData = documents.map((document) => {
         const transformedDocument = {};
         for (const key in document._doc) {
           if (fieldMapping[key]) {
             // Check if the key corresponds to quiz status
-            if (key === "isQuiz1Finished" || key === "isQuiz2Finished" || key === "isQuiz3Finished") {
-              transformedDocument[fieldMapping[key]] = document._doc[key] ? "Finished" : "Not Finished";
+            if (
+              key === "isQuiz1Finished" ||
+              key === "isQuiz2Finished" ||
+              key === "isQuiz3Finished"
+            ) {
+              transformedDocument[fieldMapping[key]] = document._doc[key]
+                ? "Finished"
+                : "Not Finished";
             } else {
               transformedDocument[fieldMapping[key]] = document._doc[key];
             }
@@ -61,68 +67,62 @@ router.get("/fetch", async (req, res) => {
   }
 });
 
-router.post('/upload', upload.single('csvFile'),(req, res) => {
-  try{
+router.post("/upload", upload.single("csvFile"), (req, res) => {
+  try {
     const csvData = req.file.buffer.toString();
-    console.log(csvData)
+    console.log(csvData);
     if (!csvData) {
-      return res.status(400).send('CSV data is required.');
-  }
-  
-  const parser = csv({ headers: true });
-  //console.log(parsedData)
-    parser
-        .on('data',async (data) => {
-          console.log('Parsed data:', data);
-          try {
-            const email = data._0;
-            const nop = parseInt(data._1);
-            // Find the user in MongoDB based on the email
-            const user = await User.findOne({ email });
+      return res.status(400).send("CSV data is required.");
+    }
 
-            if (user) {
-                // Update the NOP for the user
-                user.nop = nop;
-                await user.save(); // Save the updated user
-                console.log(`Updated NOP for user ${email} to ${nop}`);
-            } else {
-                console.log(`User with email ${email} not found.`);
-            }
-        } catch (error) {
-            console.error(`Error updating NOP for user ${email}: ${error}`);
+    const parser = csv({ headers: true });
+    //console.log(parsedData)
+    parser.on("data", async (data) => {
+      console.log("Parsed data:", data);
+      try {
+        const email = data._0;
+        const nop = parseInt(data._1);
+        // Find the user in MongoDB based on the email
+        const user = await User.findOne({ email });
+
+        if (user) {
+          // Update the NOP for the user
+          user.nop = nop;
+          await user.save(); // Save the updated user
+          console.log(`Updated NOP for user ${email} to ${nop}`);
+        } else {
+          console.log(`User with email ${email} not found.`);
         }
-            
-        });
-        parser.on('end', () => {
-          console.log('CSV data processed successfully.');
-          res.send('CSV data processed successfully.');})
+      } catch (error) {
+        console.error(`Error updating NOP for user ${email}: ${error}`);
+      }
+    });
+    parser.on("end", () => {
+      console.log("CSV data processed successfully.");
+      res.send("CSV data processed successfully.");
+    });
 
-        const Readable = require('stream').Readable;
-        const stream = new Readable();
-        stream.push(csvData);
-        stream.push(null);
-        stream.pipe(parser);
-  }
-  catch(error){
+    const Readable = require("stream").Readable;
+    const stream = new Readable();
+    stream.push(csvData);
+    stream.push(null);
+    stream.pipe(parser);
+  } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
   }
-  
 });
 
-router.post("/adminLogin", (req, res)=>{
+router.post("/adminLogin", (req, res) => {
   const { email, password } = req.body;
-  
   const predefinedEmail = "abc@gmail.com";
   const predefinedPassword = "123456";
 
   if (email === predefinedEmail && password === predefinedPassword) {
-    const token = generateRandomToken();
-    res.status(200).json({ token });
+    res.status(200).json({ token:"SOIFVRNILUWILE84HOIWH34892HJI48HGVIUH" });
   } else {
     res.status(401).json({ message: "Invalid credentials" });
   }
-  
 });
 
 module.exports = router;
